80ea3a994a41d71952b9a13e563e325d3937fe9b
AmarenaBecher <maren.mertens@stud.hshl.de>
2022-05-09 02:41:46 +0200

    nummerischer und alphabetischer bubblesort. noch nicht getestet

--- Krankenhaus/src/application/Sortieren.java
+++ Krankenhaus/src/application/Sortieren.java
@@ -4,12 +4,41 @@ public class Sortieren {
 
 	// Methoden
 
-	public static void sortiereAlphabetisch() {
+	//BubbleSort Alphabetisch
+	public static String[] sortiereAlphabetisch(String[] array) {
 
+		String memory;
+		
+		for(int j = 0; j <= array.length; j++) {
+			for(int i = 0; i <= array.length; i++) {
+				if(array[i].compareTo(array[i + 1]) > 0) {
+					memory = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = memory;
+				}
+			}
+		}
+		
+		return array;
 	}
 
-	public static void sortiereNummerisch() {
+	
+	//BubbleSort Nummerisch
+	public static int[] sortiereNummerisch(int[] array) {
 
+		int memory;
+		
+		for(int j = 0; j <= array.length; j++) {
+			for(int i = 0; i < array.length -1; i++) {
+				if(array[i] > array[i+1]) {
+					memory = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = memory;
+				}
+			}
+		}
+		
+		return array;
 	}
 
 }
