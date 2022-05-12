f63279886b1895d990a0194fb1640df4978f8bac
Norox95 <janick.metzger@stud.hshl.de>
2022-05-12 16:38:04 +0200

    Text Dateien umbenannt, Programm Start Fehler behoben

--- Krankenhaus/src/application/Main.java
+++ Krankenhaus/src/application/Main.java
@@ -5,14 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/view/AnmeldeScreen.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/gui/AnmeldeScreen.fxml"));
 			
 			Scene scene = new Scene (root, 1920, 1080);
 			
