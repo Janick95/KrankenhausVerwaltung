c3eae807c2af8ade2e1453dc87f2a5bbb976cfa2
Norox95 <janick.metzger@stud.hshl.de>
2022-06-19 17:47:28 +0200

    Search Commit

--- Krankenhaus/src/application/Main.java
+++ Krankenhaus/src/application/Main.java
@@ -192,20 +192,21 @@ public class Main extends Application {
 		
 
 		//Test/Beispiel Suchen
-		/*String[] patients = ReaderWriter.readToArray("Patienten.txt");
-		String[] nurses = ReaderWriter.readToArray("Pfleger.txt");
+		String[] patients = ReaderWriter.readToArray("Patienten.txt");
+		/*String[] nurses = ReaderWriter.readToArray("Pfleger.txt");
 		String[] doctors = ReaderWriter.readToArray("Arzt.txt");
 		String[] rooms = ReaderWriter.readToArray("Räume.txt");
-		String[] meetings = ReaderWriter.readToArray("Termine.txt");
+		String[] meetings = ReaderWriter.readToArray("Termine.txt");*/
 		
 		String[] sortedPatients1 = Sortieren.sortIDAscending(patients);
-		String[] sortedNurses1 = Sortieren.sortIDAscending(nurses);
+		/*String[] sortedNurses1 = Sortieren.sortIDAscending(nurses);
 		String[] sortedDoctors1 = Sortieren.sortIDAscending(doctors);
 		String[] sortedRooms1 = Sortieren.sortIDAscending(rooms);
-		String[] sortedMeetings1 = Sortieren.sortIDAscending(meetings);
+		String[] sortedMeetings1 = Sortieren.sortIDAscending(meetings);*/
 		
 		String searchedPatients1 = Suchen.searchID("3512", sortedPatients1);
-		String[] searchedPatients2 = Suchen.searchFirstName("Robin", patients);
+		System.out.println(searchedPatients1);
+		/*String[] searchedPatients2 = Suchen.searchFirstName("Robin", patients);
 		String[] searchedPatients3 = Suchen.searchLastName("Schmidt", patients);
 		String[] searchedPatients4 = Suchen.searchReasonForStay("Herzprobleme", patients);
 		
--- Krankenhaus/src/gui/PVController.java
+++ Krankenhaus/src/gui/PVController.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import application.Patient;
 import application.Pfleger;
 import application.ReaderWriter;
+import application.Sortieren;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -21,6 +22,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
@@ -38,24 +41,32 @@ public class PVController {
 	@FXML
 	Button btnPatDelete;
 	@FXML
+	Button btnSearch;
+	@FXML
 	TextField txtSearchPatient;
+	@FXML
+	ComboBox<String> cmbPatientSearch;
 	@FXML
 	ComboBox<String> cmbPatientSort;
 	@FXML
 	ListView<String> lvPatDat01 = new ListView<String>(); // Initialisiert die ListView mit dem Parametertyp String
 
-
-
 	@FXML
 	public void initialize() throws IOException {
 
 		// Sortieren ComboBox Inhalte:
-			cmbPatientSort.getItems().removeAll(cmbPatientSort.getItems());
-		cmbPatientSort.getItems().addAll("Sortieren nach", "ID-aufsteigend", "ID-absteigend", "Name-aufsteigend", "Name-absteigend", "Alter-aufsteigend", "Alter-absteigend", "Aufenthaltsgrund-aufsteigend", "Aufenthaltsgrund-absteigend");
+		cmbPatientSort.getItems().removeAll(cmbPatientSort.getItems());
+		cmbPatientSort.getItems().addAll("Sortieren nach", "ID-aufsteigend", "ID-absteigend", "Name-aufsteigend",
+				"Name-absteigend", "Alter-aufsteigend", "Alter-absteigend", "Aufenthaltsgrund-aufsteigend",
+				"Aufenthaltsgrund-absteigend");
 		cmbPatientSort.getSelectionModel().select("Sortieren nach");
+		
+		// Suchen ComboBox Inhalte:
+		cmbPatientSearch.getItems().removeAll(cmbPatientSort.getItems());
+		cmbPatientSearch.getItems().addAll("Suchen nach", "ID", "Vorname", "Nachname", "Aufenthaltsgrund");
+		cmbPatientSearch.getSelectionModel().select("Suchen nach");
 		String[] patients = ReaderWriter.readToArray("Patienten.txt");
 		showPatientList(patients);
-
 
 	}
 
@@ -63,32 +74,60 @@ public class PVController {
 	@FXML
 	public void showPatientList(String[] patients) throws IOException {
 
-		 // Lädt den Inhalt der übergebenen Text Datei
-																		// durch die Methode readToArray der
-																		// ReaderWriter Klasse in ein neues String Array
-		
+		// Lädt den Inhalt der übergebenen Text Datei
+		// durch die Methode readToArray der
+		// ReaderWriter Klasse in ein neues String Array
+
 		ObservableList<String> patBsp = FXCollections.observableArrayList(patients);
 		lvPatDat01.setItems(patBsp);
 	}
-	
 
-
-
-
-	
 	// Events
+	@FXML
+	public void goToMainmenu(ActionEvent event) throws IOException // Diese Methode lädt den Hauptmenübildschirm in dem
+																	// aktuellen Fenster
+	{
+		Parent root = FXMLLoader.load(getClass().getResource("/gui/HauptmenuScreen02.fxml"));
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	
+	@FXML
+	public void patientSearch(ActionEvent evt)throws IOException {
+	}
+
+	// Nach der Auswahl des gewünschten Sortierparameters wird der
+		// Sortieralgorithmus auf das Element angewandt
 		@FXML
-		public void goToMainmenu(ActionEvent event) throws IOException // Diese Methode lädt den Hauptmenübildschirm in dem
-																		// aktuellen Fenster
-		{
-			Parent root = FXMLLoader.load(getClass().getResource("/gui/HauptmenuScreen02.fxml"));
-			stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-			scene = new Scene(root);
-			stage.setScene(scene);
-			stage.show();
+		public void pickSearch(ActionEvent event) throws IOException {
+			String[] patients1 = ReaderWriter.readToArray("Patienten.txt");
+			ObservableList<String> pat = FXCollections.observableArrayList(patients1);
+			String search = txtSearchPatient.getText();
+			if (cmbPatientSearch.getValue().contains("ID")) {
+				String[] sortedPatients1 = Sortieren.sortIDAscending(patients1);
+				String searchID = application.Suchen.searchID(search, sortedPatients1);
+				pat = FXCollections.observableArrayList(searchID);
+				lvPatDat01.setItems(pat);
+			} else if (cmbPatientSearch.getValue().contains("Vorname")) {
+				String[] searchSurname = application.Suchen.searchFirstName(search, patients1);
+				pat = FXCollections.observableArrayList(searchSurname);
+				lvPatDat01.setItems(pat);
+			} else if (cmbPatientSearch.getValue().contains("Nachname")) {
+				String[] searchName = application.Suchen.searchLastName(search, patients1);
+				pat = FXCollections.observableArrayList(searchName);
+				lvPatDat01.setItems(pat);
+			} else if (cmbPatientSearch.getValue().contains("Aufenthaltsgrund")) {
+				String[] searchReason = application.Suchen.searchReasonForStay(search, patients1);
+				pat = FXCollections.observableArrayList(searchReason);
+				lvPatDat01.setItems(pat);
+			}
+
 		}
-
-
+	
+	
 	// Nach der Auswahl des gewünschten Sortierparameters wird der
 	// Sortieralgorithmus auf das Element angewandt
 	@FXML
@@ -96,43 +135,42 @@ public class PVController {
 		String[] patients1 = ReaderWriter.readToArray("Patienten.txt");
 		ObservableList<String> pat = FXCollections.observableArrayList(patients1);
 		lvPatDat01.getItems();
-			if (cmbPatientSort.getValue().contains("ID-aufsteigend")) {
-				String[] sortedID = application.Sortieren.sortIDAscending(patients1);
-				pat = FXCollections.observableArrayList(sortedID);
-				lvPatDat01.setItems(pat);
-				
-			} else if (cmbPatientSort.getValue().contains("ID-absteigend")) {
-				String[] sortedID = application.Sortieren.sortIDDescending(patients1);
-				pat = FXCollections.observableArrayList(sortedID);
-				lvPatDat01.setItems(pat);
-			} else if (cmbPatientSort.getValue().contains("Name-aufsteigend")) {
-				String[] sortedName = application.Sortieren.sortNameAscending(patients1);
-				pat = FXCollections.observableArrayList(sortedName);
-				lvPatDat01.setItems(pat);
-			} else if (cmbPatientSort.getValue().contains("Name-absteigend")) {
-				String[] sortedName = application.Sortieren.sortNameAscending(patients1);
-				pat = FXCollections.observableArrayList(sortedName);
-				lvPatDat01.setItems(pat);
-			} else if (cmbPatientSort.getValue().contains("Alter-aufsteigend")) {
-				String[] sortedID = application.Sortieren.sortIDAscending(patients1);
-				pat = FXCollections.observableArrayList(sortedID);
-				lvPatDat01.setItems(pat);
-			} else if (cmbPatientSort.getValue().contains("Alter-absteigend")) {
-				String[] sortedAge = application.Sortieren.sortAgeAscending(patients1);
-				pat = FXCollections.observableArrayList(sortedAge);
-				lvPatDat01.setItems(pat);
-			} else if (cmbPatientSort.getValue().contains("Aufenthaltsreason-aufsteigend")) {
-				String[] sortedAge = application.Sortieren.sortAgeAscending(patients1);
-				pat = FXCollections.observableArrayList(sortedAge);
-				lvPatDat01.setItems(pat);
-			} else if (cmbPatientSort.getValue().contains("Aufenthaltsgrund-absteigend")) {
-				String[] sortedReason = application.Sortieren.sortIDAscending(patients1);
-				pat = FXCollections.observableArrayList(sortedReason);
-				lvPatDat01.setItems(pat);
-			}
+		if (cmbPatientSort.getValue().contains("ID-aufsteigend")) {
+			String[] sortedID = application.Sortieren.sortIDAscending(patients1);
+			pat = FXCollections.observableArrayList(sortedID);
+			lvPatDat01.setItems(pat);
 
+		} else if (cmbPatientSort.getValue().contains("ID-absteigend")) {
+			String[] sortedID = application.Sortieren.sortIDDescending(patients1);
+			pat = FXCollections.observableArrayList(sortedID);
+			lvPatDat01.setItems(pat);
+		} else if (cmbPatientSort.getValue().contains("Name-aufsteigend")) {
+			String[] sortedName = application.Sortieren.sortNameAscending(patients1);
+			pat = FXCollections.observableArrayList(sortedName);
+			lvPatDat01.setItems(pat);
+		} else if (cmbPatientSort.getValue().contains("Name-absteigend")) {
+			String[] sortedName = application.Sortieren.sortNameAscending(patients1);
+			pat = FXCollections.observableArrayList(sortedName);
+			lvPatDat01.setItems(pat);
+		} else if (cmbPatientSort.getValue().contains("Alter-aufsteigend")) {
+			String[] sortedID = application.Sortieren.sortIDAscending(patients1);
+			pat = FXCollections.observableArrayList(sortedID);
+			lvPatDat01.setItems(pat);
+		} else if (cmbPatientSort.getValue().contains("Alter-absteigend")) {
+			String[] sortedAge = application.Sortieren.sortAgeAscending(patients1);
+			pat = FXCollections.observableArrayList(sortedAge);
+			lvPatDat01.setItems(pat);
+		} else if (cmbPatientSort.getValue().contains("Aufenthaltsreason-aufsteigend")) {
+			String[] sortedAge = application.Sortieren.sortAgeAscending(patients1);
+			pat = FXCollections.observableArrayList(sortedAge);
+			lvPatDat01.setItems(pat);
+		} else if (cmbPatientSort.getValue().contains("Aufenthaltsgrund-absteigend")) {
+			String[] sortedReason = application.Sortieren.sortIDAscending(patients1);
+			pat = FXCollections.observableArrayList(sortedReason);
+			lvPatDat01.setItems(pat);
 		}
 
+	}
 
 	// Beim klick auf Button "Neuen Patienten erstellen" wird der zweite
 	// Patientenbildschirm geladen
@@ -147,20 +185,18 @@ public class PVController {
 		stage.show();
 	}
 
-	
 	@FXML
 	public void deletePat(ActionEvent event) throws IOException {
-		
-		
+
 		String deletePat = lvPatDat01.getSelectionModel().getSelectedItem();
 		String[] patDelete = deletePat.split(",");
-	
+
 		System.out.println(deletePat);
 		application.ReaderWriter.deleteFromTxt(patDelete[0], "Patienten.txt");
-		
+
 		String[] patients = ReaderWriter.readToArray("Patienten.txt");
 		showPatientList(patients);
-		
+
 	}
 
 }
